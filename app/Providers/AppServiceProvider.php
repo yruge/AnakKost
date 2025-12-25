e11577bafb08d23dde3b484f7f36a708e1b93c9b
Jason Julius Then <131356746+yruge@users.noreply.github.com>
2025-12-25 18:46:22 +0700

    Enforce HTTPS in production environments
    Force HTTPS scheme in non-local environments.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+  public function boot()
+{
+    if(env('APP_ENV') !== 'local') {
+        URL::forceScheme('https');
     }
+}
 }
